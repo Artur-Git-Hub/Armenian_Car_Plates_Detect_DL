51119df6b3c69347cb2e17570140f8fc98e6f44d
Artur Stepanyan <78071550+Artur-Git-Hub@users.noreply.github.com>
2022-10-23 22:22:44 +0400

    Update bbox.py

--- utils/bbox.py
+++ utils/bbox.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import cv2
-from .colors import get_color
+# from .colors import get_color
 
 class BoundBox:
     def __init__(self, xmin, ymin, xmax, ymax, c = None, classes = None):
@@ -76,7 +76,7 @@ def draw_boxes(image, boxes, labels, obj_thresh, quiet=True):
                                [box.xmin+width+13, box.ymin-height-26], 
                                [box.xmin+width+13, box.ymin]], dtype='int32')  
 
-            cv2.rectangle(img=image, pt1=(box.xmin,box.ymin), pt2=(box.xmax,box.ymax), color=get_color(label), thickness=5)
+            cv2.rectangle(img=image, pt1=(box.xmin,box.ymin), pt2=(box.xmax,box.ymax), color='red', thickness=5)
             cv2.fillPoly(img=image, pts=[region], color=get_color(label))
             cv2.putText(img=image, 
                         text=label_str, 
